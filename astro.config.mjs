f2ce6fc0b7cb8111d31904616b16f328866d5761
Veronica Segovia <ft.alxx@gmail.com>
2025-12-16 23:42:14 -0500

    Changed base in config due to Repo name change

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   site: "https://ftalxx.github.io",
-  base: "/website/",
+  base: "/Portfolio/",
 });
